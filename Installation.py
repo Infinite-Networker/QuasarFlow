e65e5a4b5e9cdde20b9c03bb33e884a959112914
Dr. Ahmad Mateen Ishanzai <matt1510001@live.com>
2026-02-23 22:02:16 -0500

    Create Installation.py

---
+++ Installation.py
@@ -0,0 +1,8 @@
+# Install from the cosmic registry
+pip install quasarflow
+
+# Or clone from the galactic repository
+git clone https://github.com/yourusername/quasarflow.git
+cd quasarflow
+pip install -r requirements.txt
+python setup.py install
